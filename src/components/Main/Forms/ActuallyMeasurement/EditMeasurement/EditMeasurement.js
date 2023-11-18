8c3dd472f2bc5a75279e6c6bd301aa488ae6e167
Kiriar21 <artur.m326@gmail.com>
2023-11-18 18:43:32 +0100

    module edit measurement edited, id in link fixed

--- src/components/Main/Forms/ActuallyMeasurement/EditMeasurement/EditMeasurement.js
+++ src/components/Main/Forms/ActuallyMeasurement/EditMeasurement/EditMeasurement.js
@@ -1,12 +1,12 @@
 import {Form} from 'react-bootstrap'
 import {useState} from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import Button from '../../../../Buttons/ButtonForm/ButtonForm'
 import FormInput from '../../FormInput/FormInput'
 import Alert from '../../../../Alerts/Alert/Alert'
-import SelectMeasurement from '../../Selects/SelectMeasurement/SelectMeasurement'
 
 export default function FormNewMeasurement(props){
+    const { id } = useParams()
     const [showAlert, setShowAlert] = useState(false)
     const dateToday = new Date();
     const dateNow = dateToday.toISOString().slice(0, 10)
@@ -14,16 +14,12 @@ export default function FormNewMeasurement(props){
     const [name, setName] = useState('')
     const [city, setCity] = useState('')
     const navigate = useNavigate()
-    const options = [{
-        name:'run',
-        title:'Bieg na czas'
-    }] 
     const check = async (e) => {
         e.preventDefault()
         try{
             setShowAlert(true)
             setTimeout(() => {
-                navigate('/:id/statistic')
+                navigate(`/${id}/statistic`)
             }, 3000)
         } catch(err){
             console.log(err)
@@ -49,10 +45,6 @@ export default function FormNewMeasurement(props){
                         defaultValue={city}
                         onChange={ e => setCity(e.target.value)}
                         placeholder='Miejscowość' />
-            <SelectMeasurement controlId='selectMeasurementEdit'
-                        labelText='Typ'
-                        onChange={ e => {}}
-                        opt={ options} />
             <Button buttonTitle="Zapisz" onClick={check} />
             {showAlert  && <Alert variant='success' dismissible={false} alertContent='Zmiany zostały zapisane. Za chwile nastąpi przekierowanie na strone ze statystykami'/>}
         </Form>
